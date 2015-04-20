0d1acdd355748a4cc03f64112fec627dc6f6419d
samuel melgarejo <samuel_021093@hotmail.com>
2015-04-19 19:38:52 -0430

    primer

---
+++ Facebook_files/VJPG5CEWpYf.js
@@ -0,0 +1,5 @@
+/*!CK:3551778945!*//*1425878526,*/
+
+if (self.CavalryLogger) { CavalryLogger.start_js(["XtuGt"]); }
+
+__d("ComposerXStatusAttachmentBootloader",["ComposerXBootloadStatusAttachment","ComposerXMentionsInputReset","ComposerXPrivacyWidgetReset","MentionsInput","MentionsTypeaheadAreaView","emptyFunction"],function(a,b,c,d,e,f){b.__markCompiled&&b.__markCompiled();b('ComposerXBootloadStatusAttachment');b('ComposerXMentionsInputReset');b('ComposerXPrivacyWidgetReset');b('MentionsInput');b('MentionsTypeaheadAreaView');e.exports={emptyFunction:b('emptyFunction')};},null);
---
+++ Facebook_files/hgtW4qoUk-o.js
@@ -0,0 +1,5 @@
+/*!CK:3538874961!*//*1425878526,*/
+
+if (self.CavalryLogger) { CavalryLogger.start_js(["f0pxh"]); }
+
+__d("legacy:ComposerXControllerBootload",["ComposerXController"],function(a,b,c,d){b.__markCompiled&&b.__markCompiled();b('ComposerXController');},3);
